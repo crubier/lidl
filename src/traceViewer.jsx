8bfba0003011537968ed2ad3a7335b58a29ebc63
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2015-08-21 11:36:50 +0200

    Redimensionnement tableau

--- src/traceViewer.jsx
+++ src/traceViewer.jsx
@@ -13,15 +13,51 @@ var Table = FixedDataTable.Table;
 
 var scenarioUtils=require('./scenario.js');
 
+function testxxxx(){
+  console.log("bobobobbobobobobobobo");
+}
+
 class TraceViewer extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      tableWidth: this.props.tableWidth,
+      tableHeight: this.props.tableHeight
+    };
 // this.state = {
 //   dataList: this._dataListGetter()
 // }; // probleme this.props.listOfValues pas définit
 
   }
+
+  componentDidMount() {
+    this._updateSize();
+    var win = window;
+    if (win.addEventListener) {
+      win.addEventListener('resize', this._onResize.bind(this), false);
+    } else if (win.attachEvent) {
+      win.attachEvent('onresize', this._onResize.bind(this));
+    } else {
+      win.onresize = this._onResize.bind(this);
+    }
+  }
+
+  _onResize() {
+    clearTimeout(this._updateTimer);
+    this._updateTimer = setTimeout(this._updateSize.bind(this), 16);
+  }
+
+  _updateSize() {
+    var win = window;
+    this.setState({
+      tableWidth: win.innerWidth / 2,
+      tableHeight: win.innerHeight
+    });
+  }
+
+
+
 
 //   _dataListGetter() {
 //
@@ -134,11 +170,11 @@ class TraceViewer extends React.Component {
     return (
       <div className="TraceViewer">
 
-        <Table groupHeaderHeight={30} headerHeight={30} height={this.props.tableHeight} overflowX={controlledScrolling ? "hidden" : "auto"} overflowY={controlledScrolling ? "hidden" : "auto"} rowGetter={this._rowGetter.bind(this)} rowHeight={30} rowsCount={this.props.scenario.length} scrollLeft={this.props.left} scrollTop={this.props.top} width={650}>
+        <Table groupHeaderHeight={30} headerHeight={30} height={this.state.tableHeight} width={this.state.tableWidth} overflowX={controlledScrolling ? "hidden" : "auto"} overflowY={controlledScrolling ? "hidden" : "auto"} rowGetter={this._rowGetter.bind(this)} rowHeight={30} rowsCount={this.props.scenario.length} scrollLeft={this.props.left} scrollTop={this.props.top}>
 
           {listOfAtoms.map(function(x) {
             return (
-          <ColumnGroup fixed={true} label={x.name}>
+          <ColumnGroup key={x.name} fixed={true} label={x.name}>
             <Column dataKey={x.name} fixed={true} label={x.data.name + " " + x.direction} width={150}/>
           </ColumnGroup>
           ); })}
