2ade21f3c11e9326229a98fa825e31efffcdf990
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2015-12-08 13:18:54 +0100

    Can save graphs as svg by clicking

--- lidl-sandbox/src/Graphviz/Graphviz.js
+++ lidl-sandbox/src/Graphviz/Graphviz.js
@@ -8,6 +8,10 @@ import { ContextMenu, MenuItem, ContextMenuLayer } from "react-contextmenu";
 
 import CircularProgress  from 'material-ui/lib/circular-progress';
 
+import _  from 'lodash';
+
+import fileSaver from 'browser-filesaver';
+
 export default class Graphviz extends Component {
 
   constructor(props){
@@ -15,27 +19,30 @@ export default class Graphviz extends Component {
   }
 
   saveFile(){
-    var fileParts = [this.props.displayGraph];
+    var fileParts = [this.props.displayGraph.__html];
     var myBlob = new Blob(fileParts, {type : 'image/svg+xml'});
-
+    fileSaver.saveAs(myBlob,_.camelCase(this.props.panelName)+'.svg')
   }
 
   render() {
     if ( this.props.displayGraph ===null || this.props.displayGraph ===undefined ) {
       return  <div style={{textAlign:'center'}}><CircularProgress style={{margin:"20px"}} mode="indeterminate"  /></div>;
     } else  {
-      return (<div style={{textAlign:'center',position:'absolute',top:'5px',left:'5px',right:'5px',bottom:'5px',overflow:'none'}} dangerouslySetInnerHTML={this.props.displayGraph}/>);
+      return (<div onClick={this.saveFile.bind(this)} style={{textAlign:'center',position:'absolute',top:'5px',left:'5px',right:'5px',bottom:'5px',overflow:'none'}} dangerouslySetInnerHTML={this.props.displayGraph}/>);
     }
   }
 }
 
 
+
+
+
 //
 //
 // //Component on which context-menu must be triggred
-// const MyComponent = ContextMenuLayer("some_unique_identifier", (props) => {
+// const MyComponent = ContextMenuLayer("25bd65f1-8dd9-4bfd-b37f-603363a12184", (props) => {
 //     return {
-//         prop1: props.myProp
+//         displayGraph: props.displayGraph
 //     };
 // })(React.createClass({
 //     render() {
@@ -43,19 +50,23 @@ export default class Graphviz extends Component {
 //     }
 // }));
 //
-// const MyApp = React.createClass({
+//
+//
+// class GraphVizWithContextMenu extends Component{
 //     render() {
-//         <div>
+//         <div style={{textAlign:'center',position:'absolute',top:'5px',left:'5px',right:'5px',bottom:'5px',overflow:'none'}}>
 //             <MyComponent {...this.props}/>
 //             <MyContextMenu />
 //         </div>
 //     }
-// });
+// }
+//
+//
 //
 // //The context-menu to be triggered
-// class MyContextMenu extends Component ({
+// class MyContextMenu extends Component {
 //     render() {
-//         <ContextMenu identifier="some_unique_identifier" currentItem={this.currentItem}>
+//         <ContextMenu identifier="25bd65f1-8dd9-4bfd-b37f-603363a12184" currentItem={this.currentItem}>
 //             <MenuItem data={"some_data"} onSelect={this.handleSelect}>
 //                 ContextMenu Item 1
 //             </MenuItem>
@@ -67,8 +78,8 @@ export default class Graphviz extends Component {
 //                 ContextMenu Item 3
 //             </MenuItem>
 //         </ContextMenu>
-//     },
+//     }
 //     handleSelect(data, item) {
 //         console.log(data, item);
 //     }
-// });
+// }
