6e2e6da059f4bb799b951367cef732a6f20651fb
crubier <vincent.lecrubier@gmail.com>
2022-01-16 00:59:32 +0000

    Add lidl ts

---
+++ lidl-ts/index.test.ts
@@ -0,0 +1,433 @@
+import { assert, expect, test } from "vitest";
+import {
+  active,
+  affect,
+  allIn,
+  constant,
+  constantActive,
+  constantInactive,
+  createChannel,
+  createSink,
+  inactive,
+  noneIn,
+  print,
+  withBehaviourOut,
+  withBehaviourIn,
+  apply,
+  createSource,
+  previous,
+} from "./index";
+
+// Edit an assertion and save to see HMR in action
+
+// test('Math.sqrt()', () => {
+//   expect(Math.sqrt(4)).toBe(2)
+//   expect(Math.sqrt(144)).toBe(12)
+//   expect(Math.sqrt(2)).toBe(Math.SQRT2)
+// })
+
+// test('JSON', () => {
+//   const input = {
+//     foo: 'hello',
+//     bar: 'world',
+//   }
+
+//   const output = JSON.stringify(input)
+
+//   expect(output).eq('{"foo":"hello","bar":"world"}')
+//   assert.deepEqual(JSON.parse(output), input, 'matches original')
+// })
+
+test("affect", async () => {
+  // Utilities
+  const log = createSink();
+
+  // Interaction
+  const interaction = affect(log.send("result"), constant("toto"));
+
+  // Run
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+
+  // Trace
+  // console.log(log.content);
+  assert.deepEqual(log.content, [
+    ["result", "toto"],
+    ["result", inactive],
+    ["result", "toto"],
+    ["result", "toto"],
+  ]);
+});
+
+test("sinksourceaffect", async () => {
+  // Utilities
+  const sink = createSink();
+  const source = createSource([
+    ["yy", "toto1"],
+    ["yy", "toto2"],
+    ["yy", "toto3"],
+    ["yy", "toto4"],
+    ["yy", "toto5"],
+    ["yy", "toto6"],
+    ["yy", "toto7"],
+  ]);
+
+  // Interaction
+  const interaction = affect(sink.send("xx"), source.receive("yy"));
+
+  // Run
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  // Trace
+  // console.log(sink.content);
+  assert.deepEqual(sink.content, [
+    ["xx", "toto1"],
+    ["xx", inactive],
+    ["xx", "toto3"],
+    ["xx", "toto4"],
+    ["xx", inactive],
+    ["xx", "toto6"],
+    ["xx", "toto7"],
+  ]);
+});
+
+test("withBehaviour", async () => {
+  // Utilities
+  const log = createSink();
+
+  // Interaction
+  const interaction = withBehaviourOut(constant("toto"), log.send("behaviour"));
+
+  // Run
+  expect(await interaction()).toEqual("toto");
+  expect(await interaction()).toEqual("toto");
+
+  // Trace
+  // console.log(log.content);
+  assert.deepEqual(log.content, [
+    ["behaviour", active],
+    ["behaviour", active],
+  ]);
+});
+
+test("allIn", async () => {
+  // Utilities
+  const log = createSink();
+
+  // Interaction
+  const interaction = allIn(log.send("value1"), log.send("value2"));
+
+  // Run
+  expect(await interaction(Promise.resolve("hoy"))).toEqual(undefined);
+  expect(await interaction(Promise.resolve("ah"))).toEqual(undefined);
+
+  // Trace
+  // console.log(log.content);
+  assert.deepEqual(log.content, [
+    ["value1", "hoy"],
+    ["value2", "hoy"],
+    ["value1", "ah"],
+    ["value2", "ah"],
+  ]);
+});
+
+test("noneIn", async () => {
+  // Utilities
+  const log = createSink();
+
+  // Interaction
+  const interaction = noneIn(log.send("value1"), log.send("value2"));
+
+  // Run
+  expect(await interaction(Promise.resolve("hoy"))).toEqual(undefined);
+  expect(await interaction(Promise.resolve("ah"))).toEqual(undefined);
+
+  // Trace
+  // console.log(log.content);
+  assert.deepEqual(log.content, [
+    ["value1", inactive],
+    ["value2", inactive],
+    ["value1", inactive],
+    ["value2", inactive],
+  ]);
+});
+
+test("case0", async () => {
+  // Utilities
+  const log = createSink();
+
+  // Interaction
+  const interaction = affect(
+    log.send("result"),
+    withBehaviourOut(constant("toto"), log.send("behaviour"))
+  );
+
+  // Run
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+
+  // Trace
+  // console.log(log.content);
+  assert.deepEqual(log.content, [
+    ["behaviour", active],
+    ["result", "toto"],
+    ["behaviour", active],
+    ["result", inactive],
+    ["behaviour", active],
+    ["result", "toto"],
+    ["behaviour", active],
+    ["result", "toto"],
+  ]);
+});
+
+test("case1", async () => {
+  // Utilities
+  const yooChanel = createChannel<string>("yoo");
+
+  // Interaction
+  const interaction = withBehaviourOut(
+    yooChanel.receive(),
+    affect(yooChanel.send(), constant("Yoo"))
+  );
+
+  // Run
+  expect(await interaction()).toEqual("Yoo");
+  expect(await interaction()).toEqual("Yoo");
+  expect(await interaction()).toEqual("Yoo");
+});
+
+test("case2", async () => {
+  // Utilities
+  const yooChanel = createChannel<string>("yoo");
+  const log = createSink();
+
+  // Interaction
+  const interaction = withBehaviourIn(
+    yooChanel.send(),
+    affect(log.send("Here"), yooChanel.receive())
+  );
+
+  // Run
+  expect(await interaction(Promise.resolve("Coucou"))).toEqual(undefined);
+  expect(await interaction(Promise.resolve("hello"))).toEqual(undefined);
+  expect(await interaction(Promise.resolve("world"))).toEqual(undefined);
+
+  // Trace
+  // console.log(log.content);
+  assert.deepEqual(log.content, [
+    ["Here", "Coucou"],
+    ["Here", "hello"],
+    ["Here", "world"],
+  ]);
+});
+
+test("case3", async () => {
+  // Utilities
+  const yooChanel = createChannel<string>("yoo");
+  const log = createSink();
+
+  // Interaction
+  const interaction = withBehaviourIn(
+    yooChanel.send(),
+    allIn(
+      affect(log.send("Here"), yooChanel.receive()),
+      affect(log.send("Hey"), constant("Ho"))
+    )
+  );
+
+  // Run
+  expect(await interaction(Promise.resolve("Coucou"))).toEqual(undefined);
+  expect(await interaction(Promise.resolve("hello"))).toEqual(undefined);
+  expect(await interaction(Promise.resolve("world"))).toEqual(undefined);
+
+  // Trace
+  // console.log(log.content);
+  assert.deepEqual(log.content, [
+    ["Hey", "Ho"],
+    ["Here", "Coucou"],
+    ["Hey", "Ho"],
+    ["Here", "hello"],
+    ["Hey", "Ho"],
+    ["Here", "world"],
+  ]);
+});
+
+test("apply", async () => {
+  // Utilities
+  const sink = createSink();
+  const source = createSource([
+    ["a", 1],
+    ["a", 6],
+    ["a", 7],
+  ]);
+
+  // Interaction
+  const interaction = apply(
+    sink.send("Here"),
+    constant((x: number) => x * 2),
+    source.receive("a")
+  );
+
+  // Run
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+
+  // Trace
+  // console.log(sink.content);
+  assert.deepEqual(sink.content, [
+    ["Here", 2],
+    ["Here", 12],
+    ["Here", 14],
+  ]);
+});
+
+test("previous1", async () => {
+  // Utilities
+  const sink = createSink();
+  const source = createSource([
+    ["a", 0],
+    ["a", 1],
+    ["a", 2],
+    ["a", 3],
+    ["a", 4],
+    ["a", 5],
+    ["a", 6],
+    ["a", 7],
+    ["a", 8],
+    ["a", 9],
+  ]);
+
+  // Interaction
+  const interaction = previous(sink.send("b"), source.receive("a"));
+
+  // Run
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+
+  // Trace
+  // console.log(sink.content);
+  assert.deepEqual(sink.content, [
+    ["b", inactive],
+    ["b", 0],
+    ["b", 1],
+    ["b", 2],
+    ["b", 3],
+    ["b", 4],
+    ["b", 5],
+    ["b", 6],
+    ["b", 7],
+    ["b", 8],
+  ]);
+});
+
+test("previous2", async () => {
+  // Utilities
+  const sink = createSink();
+  const source = createSource([
+    ["a", 0],
+    ["a", 1],
+    ["a", 2],
+    ["a", 3],
+    ["a", 4],
+    ["a", 5],
+    ["a", 6],
+    ["a", 7],
+    ["a", 8],
+    ["a", 9],
+  ]);
+
+  // Interaction
+  const interaction = previous(sink.send("b"), source.receive("a"));
+
+  // Run
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+
+  // Trace
+  // console.log(sink.content);
+  assert.deepEqual(sink.content, [
+    ["b", inactive],
+    ["b", 0],
+    ["b", 1],
+    ["b", inactive],
+    ["b", 2],
+    ["b", 4],
+    ["b", inactive],
+    ["b", inactive],
+    ["b", 5],
+    ["b", 8],
+  ]);
+});
+
+test("case4", async () => {
+  // Utilities
+  const sink = createSink();
+  const source = createSource([
+    ["a", 0],
+    ["a", 1],
+    ["a", 2],
+    ["a", 3],
+    ["a", 4],
+    ["a", 5],
+    ["a", 6],
+    ["a", 7],
+    ["a", 8],
+    ["a", 9],
+  ]);
+
+  // Interaction
+  const interaction = previous(sink.send("b"), source.receive("a"));
+
+  // Run
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantInactive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+  expect(await interaction(constantActive())).toEqual(undefined);
+
+  // Trace
+  // console.log(sink.content);
+  assert.deepEqual(sink.content, [
+    ["b", inactive],
+    ["b", 0],
+    ["b", 1],
+    ["b", inactive],
+    ["b", 2],
+    ["b", 4],
+    ["b", inactive],
+    ["b", inactive],
+    ["b", 5],
+    ["b", 8],
+  ]);
+});
---
+++ lidl-ts/index.ts
@@ -0,0 +1,243 @@
+// Data values
+export const inactive = Symbol("inactive");
+export const active = Symbol("active");
+
+// Utility to create LIDL Data (types)
+export type Data<T> = typeof inactive | T;
+
+// Data
+export type Activation = Data<typeof active>;
+
+// Utility to create LIDL Interfaces
+export type In<T> = (value: Promise<Data<T>>) => Promise<void>;
+export type Out<T> = () => Promise<Data<T>>;
+
+// Channels
+export const createChannel = <T>(
+  name = "untitled"
+): { send: () => In<T>; receive: () => Out<T> } => {
+  // Both sides, in the world of LIDL
+  const senders = [];
+  const receivers = [];
+  // Both sides, in the world of JavaScript
+  let results = [];
+  let resolves = [];
+  // To run every time we ask or receive a value
+  const runSignal = () => {
+    if (
+      results.length === senders.length &&
+      resolves.length === receivers.length
+    ) {
+      // We got everything, trigger!
+      // Input algorithm here is "consensus of active inputs"
+      let result = inactive;
+      for (let i = 0; i < results.length; i++) {
+        if (results[i] !== inactive) {
+          if (result === inactive) {
+            result = results[i];
+          } else if (result === results[i]) {
+            result = results[i];
+          } else {
+            throw new Error(
+              `Multiple different values received for channel "${name}": ${results}`
+            );
+          }
+        }
+      }
+      // Input algorithm here is "all equal to result"
+      for (let j = 0; j < resolves.length; j++) {
+        resolves[j](result);
+      }
+      // Reset for next time step
+      results = [];
+      resolves = [];
+    }
+  };
+  return {
+    send: () => {
+      const sender = async (value: Promise<Data<T>>) => {
+        results.push(value);
+        runSignal();
+      };
+      senders.push(sender);
+      return sender;
+    },
+    receive: () => {
+      const receiver = () => {
+        return new Promise<Data<T>>((resolve) => {
+          resolves.push(resolve);
+          runSignal();
+        });
+      };
+      receivers.push(receiver);
+      return receiver;
+    },
+  };
+};
+
+// Util interactions
+export const constant = <T>(value: T): Out<T> => {
+  return async () => {
+    return value;
+  };
+};
+
+export const constantInactive = constant(inactive);
+export const constantActive = constant(active);
+
+export const print = <T>(): In<T> => {
+  return async (value: Promise<Data<T>>) => {
+    console.log(await value);
+  };
+};
+
+export const createSink = (initialContent = []) => {
+  const sink = {
+    content: initialContent,
+    send: <T>(prefix: string): In<T> => {
+      return async (value: Promise<Data<T>>) => {
+        sink.content.push([prefix, await value]);
+      };
+    },
+  };
+  return sink;
+};
+
+export const createSource = (initialContent = []) => {
+  const source = {
+    content: initialContent,
+    receive: <T>(prefix: string): Out<T> => {
+      let lastIndex = 0;
+      return async (): Promise<Data<T>> => {
+        while (source.content[lastIndex][0] !== prefix) {
+          if (lastIndex >= source.content.length) {
+            throw new Error(`Reached end of content for source "${prefix}"`);
+          }
+          lastIndex++;
+        }
+        return source.content[lastIndex++][1];
+      };
+    },
+  };
+  return source;
+};
+
+// Fondamental LIDL interactions
+// y = x
+export const affect = <T>(y: In<T>, x: Out<T>): In<Activation> => {
+  return async (activation: Promise<Activation>) => {
+    if ((await activation) === inactive) {
+      await Promise.all([x(), y(constantInactive())]);
+      return;
+    }
+    await y(x());
+    return;
+  };
+};
+
+// y = f (x)
+export const apply = <X, Y>(
+  y: In<Y>,
+  f: Out<(input: X) => Y | Promise<Y>>,
+  x: Out<X>
+): In<Activation> => {
+  return async (activation: Promise<Activation>) => {
+    if ((await activation) === inactive) {
+      await Promise.all([x(), f(), y(constantInactive())]);
+      return;
+    }
+    const xValue = await x();
+    if (xValue === inactive) {
+      await Promise.all([f(), y(constantInactive())]);
+      return;
+    }
+    const fValue = await f();
+    if (fValue === inactive) {
+      await y(constantInactive());
+      return;
+    }
+    const yValue = await fValue(xValue);
+    await y(Promise.resolve(yValue));
+    return;
+  };
+};
+
+// y = previous x
+export const previous = <T>(y: In<T>, x: Out<T>): In<Activation> => {
+  let lastValue: Data<T> = inactive;
+  return async (activation: Promise<Activation>) => {
+    if ((await activation) === inactive) {
+      await Promise.all([x(), y(Promise.resolve(inactive))]);
+      return;
+    }
+    [lastValue] = await Promise.all([x(), y(Promise.resolve(lastValue))]);
+    return;
+  };
+};
+
+export const withBehaviourIn = <T>(a: In<T>, b: In<Activation>): In<T> => {
+  return async (value: Promise<Data<T>>) => {
+    await Promise.all([a(value), b(Promise.resolve(active))]);
+    return;
+  };
+};
+
+export const withBehaviourOut = <T>(a: Out<T>, b: In<Activation>): Out<T> => {
+  return async () => {
+    const [result] = await Promise.all([a(), b(Promise.resolve(active))]);
+    return result;
+  };
+};
+
+export const allIn = <T>(...interactions: Array<In<T>>): In<T> => {
+  return async (value: Promise<Data<T>>) => {
+    const promises = [];
+    for (const interaction of interactions) {
+      promises.push(interaction(value));
+    }
+    await Promise.all(promises);
+    return;
+  };
+};
+
+export const someIn = <T>(...interactions: Array<In<T>>): In<T> => {
+  return async (value: Promise<Data<T>>) => {
+    const promises = [];
+    for (const interaction of interactions) {
+      if (Math.random() >= 0.5) {
+        promises.push(interaction(value));
+      } else {
+        promises.push(interaction(constantInactive()));
+      }
+    }
+    await Promise.all(promises);
+    return;
+  };
+};
+
+export const eitherIn = <T>(...interactions: Array<In<T>>): In<T> => {
+  return async (value: Promise<Data<T>>) => {
+    const index = Math.floor(Math.random() * interactions.length);
+    const promises = [];
+    for (let i = 0; i < interactions.length; i++) {
+      if (i === index) {
+        promises.push(interactions[i](value));
+      } else {
+        promises.push(interactions[i](constantInactive()));
+      }
+    }
+    await Promise.all(promises);
+    return;
+  };
+};
+
+export const noneIn = <T>(...interactions: Array<In<T>>): In<T> => {
+  return async () => {
+    const promises = [];
+    for (const interaction of interactions) {
+      promises.push(interaction(constantInactive()));
+    }
+    await Promise.all(promises);
+    return;
+  };
+};
