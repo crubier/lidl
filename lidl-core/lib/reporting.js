58c39ae34e39a4f1596f4c9aea75a0c6d7fa3a91
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2026-03-09 19:04:38 -0700

    Remove old JS stuff

--- lidl-core/lib/reporting.js
+++
@@ -1,5 +0,0 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.
-
-printInteractionInstanceNodeStack = printInteractionInstanceNodeStack;var _serializer = require('./serializer');function printInteractionInstanceNodeStack(node) {
-  return node.content.meta.stack.map(function (x) {return 'at ' + (0, _serializer.serialize)(x.content) + " (" + x.content.meta.location.start.line + ":" + x.content.meta.location.start.column + ")";}).join('\n');
-}
